cd711edb9adb7b5aae7af867138ecbf4daa82786
ThomasDLi <lithomas753@gmail.com>
2024-11-05 14:09:17 -0800

    added testing module & wrote some parts of the readme

--- quantrack/main.py
+++ quantrack/main.py
@@ -1,3 +1,6 @@
+# to run the server: uvicorn quantrack.main:app --reload
+# or python -m quantrack.main (to run without auto-reload)
+
 from fastapi import Depends, FastAPI
 
 # from quantrack.config import settings
@@ -9,7 +12,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Bigger Applications!"}
+    return {"message": "Hello World"}
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
